4aa2e79f49b4d40c95bed8c0d2418a0bfb84d809
Akhilvalluru1 <109025277+AkhilV14@users.noreply.github.com>
2023-05-20 20:37:29 +0530

    first commit

---
+++ Akhil/my-app/src/Akhil.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Akhil() {
+    return (
+        // <div>Akhil</div>
+        <div className='center'>
+            {/* <h1>Akhil kumar</h1> */}
+            <header>
+                <center>
+                    <h1>React App</h1>
+                    <a href="second.html">Second</a>
+                    <a href="https://www.facebook.com/aakash.handa.14/" target="_blank">
+                        {/* <img src="images/facebook.png" alt="fb" height="50" width="50"> */}
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank">
+                        {/* <img src="images/insta.png" alt="fb" height="50" width="50"> */}
+                    </a>
+                </center>
+                <hr />
+            </header>
+        </div>
+    )
+}
+
+export default Akhil
---
+++ Akhil/my-app/src/Akhil1.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Akhil1 extends Component {
+    render() {
+        return (
+            //   <div>Akhil1</div>
+            <div>
+                <header>
+                    <center>
+                        <h1>React App</h1>
+                        <a href="second.html">Second</a>
+                        <a href="https://www.facebook.com/aakash.handa.14/" target="_blank">
+                            {/* <img src="images/facebook.png" alt="fb" height="50" width="50"> */}
+                        </a>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            {/* <img src="images/insta.png" alt="fb" height="50" width="50"> */}
+                        </a>
+                    </center>
+                    <hr />
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Akhil1
